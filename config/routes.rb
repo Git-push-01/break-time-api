3644f5d92d4dc9c6b89e5e32c3f295bb49236e52
hugo escandon <hescandon6@gmail.com>
2019-05-29 14:54:10 -0400

    second heroku commit 2.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-      namespace :v1 do
+
         resources :users
         get 'user', to: 'users#show', as: 'user_show'
       post 'signup', to: 'users#create', as: 'user_signup'
@@ -12,7 +11,7 @@ Rails.application.routes.draw do
   resources :managers
   resources :companies
 
-  
+
 
 end
 end
