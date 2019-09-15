34119218ead886e2e4d6469d216b5b5319109ecc
hugo escandon <hescandon6@gmail.com>
2019-09-15 11:08:53 -0400

    -datadog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,15 +17,6 @@ class ApplicationController < ActionController::API
 
       user
     end
-    def append_info_to_payload(payload)
-      super
-     case
-        when payload[:status] == 200
-          payload[:level] = "INFO"
-        when payload[:status] == 302
-          payload[:level] = "WARN"
-        else
-          payload[:level] = "ERROR"
-        end
-end
+
+
   end
